ccb9f6bc474fe4312f25a43ca6395bef020e2b3f
Reiichiro Nakano <reiichiro.s.nakano@gmail.com>
2019-10-24 19:48:04 +0900

    add transformer architecture used by deepmind math paepr

--- fairseq/models/transformer.py
+++ fairseq/models/transformer.py
@@ -749,6 +749,19 @@ def transformer_iwslt_de_en(args):
     base_architecture(args)
 
 
+@register_model_architecture('transformer', 'transformer_math')
+def transformer_iwslt_de_en(args):
+    args.encoder_embed_dim = getattr(args, 'encoder_embed_dim', 512)
+    args.encoder_ffn_embed_dim = getattr(args, 'encoder_ffn_embed_dim', 2048)
+    args.encoder_attention_heads = getattr(args, 'encoder_attention_heads', 8)
+    args.encoder_layers = getattr(args, 'encoder_layers', 6)
+    args.decoder_embed_dim = getattr(args, 'decoder_embed_dim', 512)
+    args.decoder_ffn_embed_dim = getattr(args, 'decoder_ffn_embed_dim', 2048)
+    args.decoder_attention_heads = getattr(args, 'decoder_attention_heads', 8)
+    args.decoder_layers = getattr(args, 'decoder_layers', 6)
+    base_architecture(args)
+
+
 @register_model_architecture('transformer', 'transformer_wmt_en_de')
 def transformer_wmt_en_de(args):
     base_architecture(args)
