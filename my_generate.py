64b1ca5eae6cdb2452b75f8ca9ce4d4ead244f15
Reiichiro Nakano <reiichiro.s.nakano@gmail.com>
2019-10-26 16:53:12 +0900

    shuffle test set

--- my_generate.py
+++ my_generate.py
@@ -77,7 +77,7 @@ def main(args):
         num_shards=args.num_shards,
         shard_id=args.shard_id,
         num_workers=args.num_workers,
-    ).next_epoch_itr(shuffle=False)
+    ).next_epoch_itr(shuffle=True)
 
     # Initialize generator
     num_sentences = 0
