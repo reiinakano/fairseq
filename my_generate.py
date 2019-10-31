e9f792e90ebf7335e3b7408a43a6cf2fc96c4169
Reiichiro Nakano <reiichiro.s.nakano@gmail.com>
2019-10-31 13:20:25 +0900

    ATTNDEBUG

--- my_generate.py
+++ my_generate.py
@@ -138,6 +138,8 @@ def main(args):
                     while prev_output_tokens_list[-1] != tgt_dict.eos() or len(prev_output_tokens_list) == 1:
                         prev_output_tokens = torch.LongTensor([prev_output_tokens_list]).to(encoder_out['encoder_out'].device)
                         decoder_out = model.decoder.forward(prev_output_tokens, encoder_out)
+                        print(decoder_out)
+                        raise
                         decoder_out = decoder_out[0][0][token_idx]
                         #print('decoder output shape', decoder_out.shape)
                         top_indices = decoder_out.argsort(descending=True)
