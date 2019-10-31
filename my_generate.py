9518d50688b0883936af0872b2f7543cc337c5f8
Reiichiro Nakano <reiichiro.s.nakano@gmail.com>
2019-10-31 13:22:43 +0900

    ATTNDEBUG

--- my_generate.py
+++ my_generate.py
@@ -138,7 +138,7 @@ def main(args):
                     while prev_output_tokens_list[-1] != tgt_dict.eos() or len(prev_output_tokens_list) == 1:
                         prev_output_tokens = torch.LongTensor([prev_output_tokens_list]).to(encoder_out['encoder_out'].device)
                         decoder_out = model.decoder.forward(prev_output_tokens, encoder_out)
-                        print(decoder_out)
+                        print(decoder_out[1]['attn'], print(decoder_out[1]['attn'].shape))
                         raise
                         decoder_out = decoder_out[0][0][token_idx]
                         #print('decoder output shape', decoder_out.shape)
