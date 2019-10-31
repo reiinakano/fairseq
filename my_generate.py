9226c73dff675e9cca39fe3cce32402146d566b9
Reiichiro Nakano <reiichiro.s.nakano@gmail.com>
2019-10-31 22:38:00 +0900

    more beam search

--- my_generate.py
+++ my_generate.py
@@ -195,7 +195,7 @@ def main(args):
                                         except SyntaxError:
                                             print('errored syntax')
                                             continue
-                                        new_token_sequence += list(calculated_result)
+                                        new_token_sequence += map(tgt_dict.index, list(calculated_result))
 
                                     new_log_prob = seq.logprob + decoder_out[top_indices[i]].item()
                                     sequences_to_be_ranked.append(Sequence(tokens=new_token_sequence, logprob=new_log_prob))
